a6220e9c551bae134833a27c9895d8f7723a822b
d0rj_3.22.8 <dima.balobin_barnaul@mail.ru>
2020-05-11 20:37:32 +0300

    Added Flip function for arrays and vectors.

---
+++ Arrays/Utils.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vector>
+
+
+namespace AlgoShiz
+{
+	template <typename T>
+	void Flip(std::vector<T>* arr)
+	{
+		for (size_t i = 0; i < arr->size() / 2; ++i)
+			std::swap(arr->at(i), arr->at(arr->size() - i - 1));
+	}
+
+
+	template <typename T>
+	std::vector<T> Fliped(const std::vector<T>* arr)
+	{
+		std::vector<T> result(*arr);
+			
+		for (size_t i = 0; i < result.size() / 2; ++i)
+			std::swap(result[i], result[result.size() - i - 1]);
+
+		return result;
+	}
+
+
+	template <typename T>
+	void Flip(T* arr, size_t n)
+	{
+		for (size_t i = 0; i < n / 2; ++i)
+			std::swap(arr[i], arr[n - i - 1]);
+	}
+
+
+	template <typename T>
+	T* Fliped(const T* arr, size_t n)
+	{
+		T* result = new T[sizeof(T) * n];
+		std::copy(arr, arr + n, result);
+
+		for (size_t i = 0; i < n / 2; ++i)
+			std::swap(result[i], result[n - i - 1]);
+
+		return result;
+	}
+}
