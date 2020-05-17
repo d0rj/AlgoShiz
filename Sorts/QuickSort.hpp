d142e199d422e1e1aeafaf1bd21727dbc5f35ca4
d0rj_3.22.8 <dima.balobin_barnaul@mail.ru>
2020-05-17 21:14:05 +0300

    Added Quick sort only for simple arrays.

---
+++ Sorts/QuickSort.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+
+
+namespace AlgoShiz
+{
+    template<class T>
+    void QuickSort(T* arr, long n)
+    {
+        long i = 0;
+        long j = n - 1;
+        T temp, p;
+
+        p = arr[n >> 1];
+
+        do
+        {
+            while (arr[i] < p) 
+                ++i;
+            while (arr[j] > p) 
+                --j;
+
+            if (i <= j)
+                std::swap(arr[i++], arr[j--]);
+        } 
+        while (i <= j);
+
+        if (j > 0) 
+            QuickSort(arr, j + 1);
+        if (n > i) 
+            QuickSort(arr + i, n - i);
+    }
+}
