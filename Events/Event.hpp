a074dcad5559f4ab854a9183f43d649a05099427
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-05-26 20:41:31 +0300

    Added simple event system

---
+++ Events/Event.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+
+#include <vector>
+#include <functional>
+
+
+template <typename Ret, typename ...Args>
+using delegate = std::function<Ret(Args...)>;
+
+
+template <typename Ret, typename ...Args>
+class Event
+{
+private:
+	using fn_t = delegate<Ret, Args...>;
+	std::vector<fn_t> observers;
+public:
+	void AddHandler(fn_t f)
+	{
+		observers.push_back(f);
+	}
+
+	void RemoveHandler(fn_t f)
+	{
+		auto place = std::find(observers.begin(), observers.end(), f);
+		if (place != observers.end())
+			observers.erase(observers.begin() + place);
+	}
+
+	void operator ()(Args... a)
+	{
+		Invoke(a...);
+	}
+
+	void Invoke(Args... a)
+	{
+		for (auto observer : observers)
+			observer(a...);
+	}
+};
