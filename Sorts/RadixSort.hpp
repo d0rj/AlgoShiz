ed15d9e952014649155e3c170c252991ea5f439e
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-05-30 21:23:42 +0300

    Added radix sort for int arrays.

---
+++ Sorts/RadixSort.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <cmath>
+#include <string.h>
+
+
+namespace AlgoShiz
+{
+	/// <summary> O(n) </summary>
+	void RadixSort(int a[], int n)
+	{
+		int count[10];
+		int* output = new int[n];
+
+		memset(output, 0, sizeof(output));
+		memset(count, 0, sizeof(count));
+
+		int max = 0;
+		for (int i = 0; i < n; ++i)
+			if (a[i] > max)
+				max = a[i];
+
+		int maxdigits = 0;
+		while (max)
+		{
+			++maxdigits;
+			max /= 10;
+		}
+
+		for (size_t j = 0; j < maxdigits; ++j)
+		{
+			for (size_t i = 0; i < n; ++i)
+			{
+				int t = std::pow(10, j);
+				++count[(a[i] % (10 * t)) / t];
+			}
+
+			int k = 0;
+			for (int p = 0; p < 10; ++p)
+			{
+				for (int i = 0; i < n; ++i)
+				{
+					int t = std::pow(10, j);
+
+					if ((a[i] % (10 * t)) / t == p)
+						output[k++] = a[i];
+				}
+			}
+
+			memset(count, 0, sizeof(count));
+
+			for (int i = 0; i < n; ++i)
+				a[i] = output[i];
+		}
+	}
+}
