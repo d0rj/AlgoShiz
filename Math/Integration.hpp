ff17739cbbb2d951ca2980e16651ddfa8c818811
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-06-05 09:44:56 +0300

    Added Simpson's integration for one variable function.

--- Math/Integration.hpp
+++ Math/Integration.hpp
@@ -3,7 +3,7 @@
 #include <functional>
 
 
-namespace AlgoShiz
+namespace AlgoShiz 
 {
 	const size_t DefaultPanelCount = 1000;
 
@@ -22,6 +22,42 @@ namespace AlgoShiz
 		double result = 0;
 		for (double i = a; i < b; i += step)
 			result += trapezoidSquare(f(i), f(i + step), step);
+
+		return result;
+	}
+
+
+	double paraboloidSquare(std::function<double(double)> f, double a, double b)
+	{
+		double m = (a + b) / 2.0f;
+		return (b - a) / 6.0f * (f(a) + 4 * f(m) + f(b));
+	}
+
+
+	double IntegralSimpsons(std::function<double(double)> f, double a, double b, size_t fragmentation = 0)
+	{
+		if (fragmentation == 0)
+			fragmentation = DefaultPanelCount;
+
+		double h = (b - a) / fragmentation;
+		double x_i = 0;
+		double fx_i = 0;
+
+		double result = 0;
+		for (size_t i = 0; i <= fragmentation; ++i)
+		{
+			x_i = a + i * h;
+			fx_i = f(x_i);
+
+			if (i == 0 || i == fragmentation)
+				result += fx_i;
+			else if (i % 2 != 0)
+				result += 4 * fx_i;
+			else
+				result += 2 * fx_i;
+		}
+
+		result *= (h / 3);
 
 		return result;
 	}
