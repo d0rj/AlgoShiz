f9caab356c9e7072a779f8dfb51ef6d0f7851bbb
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-10-31 22:53:43 +0300

    Added n-th root of number.

--- Math/Sqrt.hpp
+++ Math/Sqrt.hpp
@@ -27,4 +27,33 @@ namespace AlgoShiz
 
         return -1;
 	}
+
+
+    /// <summary> n-th root of a </summary>
+    /// <param name="a"> Value eto root </param>
+    /// <param name="n"> Root degree </param>
+    long double Root(long double a, int n)
+    {
+        if (n == 0)
+            return 1;
+
+        if (n < 0)
+            return Root(a, -n);
+
+        long double eps = 1e-5;
+        long double root = a / n;
+        long double rn = a;
+
+        while (abs(root - rn) >= eps)
+        {
+            rn = a;
+
+            for (size_t i = 1; i < n; ++i)
+                rn = rn / root;
+
+            root = 0.5 * (rn + root);
+        }
+
+        return root;
+    }
 }
