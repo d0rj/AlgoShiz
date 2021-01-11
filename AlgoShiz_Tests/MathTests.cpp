6b26bcf23a9139367d61662272947bf6be752ae4
d0rj <hanaan@internet.ru>
2021-01-11 21:30:23 +0300

    Added utils test.

--- AlgoShiz_Tests/MathTests.cpp
+++ AlgoShiz_Tests/MathTests.cpp
@@ -74,4 +74,44 @@ namespace AlgoShizTests
 			Assert::AreEqual(0.5, Root(4, -2));
 		}
 	};
+
+
+	TEST_CLASS(UtilsTests)
+	{
+	public:
+
+		TEST_METHOD(absTest)
+		{
+			Assert::AreEqual(2, AlgoShiz::abs(-2));
+			Assert::AreEqual(2, AlgoShiz::abs(2));
+			Assert::AreEqual(0, AlgoShiz::abs(0));
+
+			Assert::AreEqual(2.0, AlgoShiz::abs(-2.0));
+			Assert::AreEqual(2.0f, AlgoShiz::abs(-2.0f));
+		}
+
+
+		TEST_METHOD(minTest)
+		{
+			Assert::AreEqual(1, AlgoShiz::min(1, 2));
+			Assert::AreEqual(1, AlgoShiz::min(2, 1));
+
+			Assert::AreEqual(-2, AlgoShiz::min(-2, -1));
+			Assert::AreEqual(-2, AlgoShiz::min(-1, -2));
+
+			Assert::AreEqual(1, AlgoShiz::min(1, 1));
+		}
+
+
+		TEST_METHOD(maxTest)
+		{
+			Assert::AreEqual(2, AlgoShiz::max(1, 2));
+			Assert::AreEqual(2, AlgoShiz::max(2, 1));
+
+			Assert::AreEqual(-1, AlgoShiz::max(-2, -1));
+			Assert::AreEqual(-1, AlgoShiz::max(-1, -2));
+
+			Assert::AreEqual(1, AlgoShiz::max(1, 1));
+		}
+	};
 }
