dfe7134f87d7091c503497390331c40108ab3d86
d0rj <hanaan@internet.ru>
2021-01-11 22:00:04 +0300

    Added tests for prime check and factorization function.

--- AlgoShiz_Tests/MathTests.cpp
+++ AlgoShiz_Tests/MathTests.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../AlgoShiz/Math/Division.hpp"
+#include "../AlgoShiz/Math/Factorization.hpp"
 #include "../AlgoShiz/Math/Power.hpp"
 #include "../AlgoShiz/Math/Sqrt.hpp"
 #include "../AlgoShiz/Math/Utils.hpp"
@@ -137,6 +138,55 @@ namespace AlgoShizTests
 			Assert::AreEqual(445, ModeExp(4, 13, 497));
 			Assert::AreEqual(342, ModeExp(595, 703, 991));
 			Assert::AreEqual(3, ModeExp(175, 235, 257));
+		}
+	};
+
+
+	TEST_CLASS(FactorizationTests)
+	{
+	public:
+		
+		TEST_METHOD(IsPrimeTest)
+		{
+			Assert::IsTrue(IsPrime(1));
+			Assert::IsTrue(IsPrime(2));
+			Assert::IsTrue(IsPrime(3));
+			Assert::IsTrue(IsPrime(5));
+			Assert::IsTrue(IsPrime(13));
+
+			Assert::IsFalse(IsPrime(6));
+			Assert::IsFalse(IsPrime(42));
+
+			Assert::IsFalse(IsPrime(-1));
+		}
+
+
+		TEST_METHOD(FactorizeTest)
+		{
+			std::vector<int> right = { 2, 3, 5, 5 };
+			int num = 2 * 3 * 5 * 5;
+
+			Assert::IsTrue(right == Factorize(num));
+
+			right = { 2, 2, 2, 2 };
+			num = 2 * 2 * 2 * 2;
+
+			Assert::IsTrue(right == Factorize(num));
+
+			right = {  };
+			num = 1;
+
+			Assert::IsTrue(right == Factorize(num));
+
+			right = { 73 };
+			num = 73;
+
+			Assert::IsTrue(right == Factorize(num));
+
+			right = { -1, 2, 3 };
+			num = -1 * 2 * 3;
+
+			Assert::IsTrue(right == Factorize(num));
 		}
 	};
 }
