1e30a111496da72b31f7b041694fb377f6fe7cce
d0rj <hanaan@internet.ru>
2021-01-21 18:01:58 +0300

    Added tests for moving average function and it was reworked.

--- AlgoShiz/Math/Average.hpp
+++ AlgoShiz/Math/Average.hpp
@@ -1,50 +1,23 @@
 #pragma once
 
-#include "Division.hpp"
+#include <cmath>
 
 
 namespace AlgoShiz
 {
 	double* MovingAverage(double* f, size_t n, size_t window)
 	{
-		double* result = new double[n];
-        int z, k1, k2, hw;
-        double tmp;
+		double* result = new double[n - window + 1];
+        
+		for (size_t i = 0; i < n - window + 1; ++i)
+		{
+			double sum = 0;
+			for (size_t j = i; j < i + window; ++j)
+				sum += f[j];
 
-        if (fmod(window, 2) == 0) 
-            window++;
+			result[i] = sum / window;
+		}
 
-        hw = (window - 1) / 2;
-        result[0] = f[0];
-
-        for (int i = 1; i < n; ++i)
-        {
-            tmp = 0;
-            if (i < hw)
-            {
-                k1 = 0;
-                k2 = 2 * i;
-                z = k2 + 1;
-            }
-            else if ((i + hw) > (n - 1))
-            {
-                k1 = i - n + i + 1;
-                k2 = n - 1;
-                z = k2 - k1 + 1;
-            }
-            else
-            {
-                k1 = i - hw;
-                k2 = i + hw;
-                z = window;
-            }
-
-            for (int j = k1; j <= k2; j++)
-                tmp = tmp + f[j];
-
-            result[i] = tmp / z;
-        }
-
-        return result;
+		return result;
 	}
 }
---
+++ AlgoShiz_Tests/Math/Average.cpp
@@ -0,0 +1,44 @@
+#include "CppUnitTest.h"
+#include "../../AlgoShiz/Math/Average.hpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace AlgoShiz;
+
+
+namespace AlgoShizTests
+{
+	TEST_CLASS(AverageTests)
+	{
+	public:
+
+		TEST_METHOD(MovingAverageTest)
+		{
+			double* input = new double[] { 1, 2, 3, 4 };
+			size_t size = 4;
+
+			double* expect = new double[] { 3.0 / 2, 5.0 / 2, 7.0 / 2 };
+			double* output = MovingAverage(input, size, 2);
+			for (size_t i = 0; i < 3; ++i)
+				Assert::AreEqual(expect[i], output[i]);
+
+			input = new double[] { 1, 2, 3, 4 };
+			expect = new double[] { 2, 3 };
+			output = MovingAverage(input, size, 3);
+			for (size_t i = 0; i < 2; ++i)
+				Assert::AreEqual(expect[i], output[i]);
+
+			input = new double[] { 1, 2, 3, 4 };
+			expect = new double[] { 5.0 / 2 };
+			output = MovingAverage(input, size, 4);
+			for (size_t i = 0; i < 1; ++i)
+				Assert::AreEqual(expect[i], output[i]);
+
+			input = new double[] { 0, 0, 0, 0 };
+			expect = new double[] { 0, 0 };
+			output = MovingAverage(input, size, 3);
+			for (size_t i = 0; i < 1; ++i)
+				Assert::AreEqual(expect[i], output[i]);
+		}
+	};
+}
