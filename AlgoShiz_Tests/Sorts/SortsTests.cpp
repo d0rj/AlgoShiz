82ee70beadd482c768690a75992505f57d36c1f1
d0rj <hanaan@internet.ru>
2021-02-08 18:54:01 +0300

    Added tests for bubble sort.

---
+++ AlgoShiz_Tests/Sorts/SortsTests.cpp
@@ -0,0 +1,35 @@
+#include "CppUnitTest.h"
+#include "../../AlgoShiz/Sorts/BubbleSort.hpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace AlgoShiz;
+
+
+namespace AlgoShizTests
+{
+	TEST_CLASS(SortsTests)
+	{
+	public:
+		TEST_METHOD(BubbleSortedTest)
+		{
+			auto unsorted = std::vector<int>{ 3, 2, 6, 1, 5, 4 };
+			auto sorted = std::vector<int>{ 1, 2, 3, 4, 5, 6 };
+			std::vector<int> result = BubbleSorted(&unsorted);
+
+			for (size_t i = 0; i < 6; ++i)
+				Assert::AreEqual(sorted[i], result[i]);
+		}
+
+
+		TEST_METHOD(BubbleSortTest)
+		{
+			auto unsorted = std::vector<int>{ 3, 2, 6, 1, 5, 4 };
+			auto sorted = std::vector<int>{ 1, 2, 3, 4, 5, 6 };
+			BubbleSort(&unsorted);
+
+			for (size_t i = 0; i < 6; ++i)
+				Assert::AreEqual(sorted[i], unsorted[i]);
+		}
+	};
+}
