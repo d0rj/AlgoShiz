c1a264095dfc13bd70e6242c31c84faa1342a417
d0rj <hanaan@internet.ru>
2021-02-10 23:46:16 +0300

    Changed tests for searching; added tests for binary search.

--- AlgoShiz_Tests/Search/SearchesTests.cpp
+++ AlgoShiz_Tests/Search/SearchesTests.cpp
@@ -15,46 +15,46 @@ namespace Tests_Searches
 	private:
 		void testWithArray(std::function<int(int*, size_t, int)> search)
 		{
-			int* arr = new int[10]{ 2, 4, 9, 11, 1, 3, 6, 7, 5, 1 };
+			int* arr = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 			auto findedIndex = search(arr, 10, 11);
-			Assert::AreEqual(3, findedIndex);
-
-			findedIndex = search(arr, 10, -10);
 			Assert::AreEqual(-1, findedIndex);
 
+			findedIndex = search(arr, 10, 4);
+			Assert::AreEqual(3, findedIndex);
+
 			findedIndex = search(arr, 10, 1);
-			Assert::AreEqual(4, findedIndex);
+			Assert::AreEqual(0, findedIndex);
 		}
 
 
 		void testWithVector(std::function<int(std::vector<int>&, int)> search)
 		{
-			std::vector<int> arr = { 2, 4, 9, 11, 1, 3, 6, 7, 5, 1 };
+			std::vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 			auto findedIndex = search(arr, 11);
-			Assert::AreEqual(3, findedIndex);
-
-			findedIndex = search(arr, -10);
 			Assert::AreEqual(-1, findedIndex);
 
+			findedIndex = search(arr, 4);
+			Assert::AreEqual(3, findedIndex);
+
 			findedIndex = search(arr, 1);
-			Assert::AreEqual(4, findedIndex);
+			Assert::AreEqual(0, findedIndex);
 		}
 
 
 		void testWithVectorPointer(std::function<int(std::vector<int>*, int)> search)
 		{
-			std::vector<int> arr = { 2, 4, 9, 11, 1, 3, 6, 7, 5, 1 };
+			std::vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 			auto findedIndex = search(&arr, 11);
-			Assert::AreEqual(3, findedIndex);
-
-			findedIndex = search(&arr, -10);
 			Assert::AreEqual(-1, findedIndex);
 
+			findedIndex = search(&arr, 4);
+			Assert::AreEqual(3, findedIndex);
+
 			findedIndex = search(&arr, 1);
-			Assert::AreEqual(4, findedIndex);
+			Assert::AreEqual(0, findedIndex);
 		}
 
 	public:
@@ -85,6 +85,39 @@ namespace Tests_Searches
 			auto testing = [](std::vector<int>* arr, int elem) -> int
 			{
 				return LinearSearch(arr, elem);
+			};
+
+			testWithVectorPointer(testing);
+		}
+
+
+		TEST_METHOD(BinarySearchTest_Array)
+		{
+			auto testing = [](int* arr, size_t n, int elem) -> int
+			{
+				return BinarySearch(arr, n, elem);
+			};
+
+			testWithArray(testing);
+		}
+
+
+		TEST_METHOD(BinarySearchTest_Vector)
+		{
+			auto testing = [](std::vector<int>& arr, int elem) -> int
+			{
+				return BinarySearch(arr, elem);
+			};
+
+			testWithVector(testing);
+		}
+
+
+		TEST_METHOD(BinarySearchTest_VectorPointer)
+		{
+			auto testing = [](std::vector<int>* arr, int elem) -> int
+			{
+				return BinarySearch(arr, elem);
 			};
 
 			testWithVectorPointer(testing);
