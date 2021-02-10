ae3a8558fb4e00d5eba1316b7500aa907956dba0
d0rj <hanaan@internet.ru>
2021-02-10 23:38:17 +0300

    Changed testing funcitons.

--- AlgoShiz_Tests/Search/SearchesTests.cpp
+++ AlgoShiz_Tests/Search/SearchesTests.cpp
@@ -1,4 +1,5 @@
 #include "CppUnitTest.h"
+#include <functional>
 #include "../../AlgoShiz/Search/LinearSearch.hpp"
 #include "../../AlgoShiz/Search/BinarySearch.hpp"
 
@@ -11,49 +12,82 @@ namespace Tests_Searches
 {
 	TEST_CLASS(SearchesTests)
 	{
+	private:
+		void testWithArray(std::function<int(int*, size_t, int)> search)
+		{
+			int* arr = new int[10]{ 2, 4, 9, 11, 1, 3, 6, 7, 5, 1 };
+
+			auto findedIndex = search(arr, 10, 11);
+			Assert::AreEqual(3, findedIndex);
+
+			findedIndex = search(arr, 10, -10);
+			Assert::AreEqual(-1, findedIndex);
+
+			findedIndex = search(arr, 10, 1);
+			Assert::AreEqual(4, findedIndex);
+		}
+
+
+		void testWithVector(std::function<int(std::vector<int>&, int)> search)
+		{
+			std::vector<int> arr = { 2, 4, 9, 11, 1, 3, 6, 7, 5, 1 };
+
+			auto findedIndex = search(arr, 11);
+			Assert::AreEqual(3, findedIndex);
+
+			findedIndex = search(arr, -10);
+			Assert::AreEqual(-1, findedIndex);
+
+			findedIndex = search(arr, 1);
+			Assert::AreEqual(4, findedIndex);
+		}
+
+
+		void testWithVectorPointer(std::function<int(std::vector<int>*, int)> search)
+		{
+			std::vector<int> arr = { 2, 4, 9, 11, 1, 3, 6, 7, 5, 1 };
+
+			auto findedIndex = search(&arr, 11);
+			Assert::AreEqual(3, findedIndex);
+
+			findedIndex = search(&arr, -10);
+			Assert::AreEqual(-1, findedIndex);
+
+			findedIndex = search(&arr, 1);
+			Assert::AreEqual(4, findedIndex);
+		}
+
 	public:
 		TEST_METHOD(LinearSearchTest_Array)
 		{
-			int* arr = new int[10] { 2, 4, 9, 11, 1, 3, 6, 7, 5, 1 };
+			auto testing = [](int* arr, size_t n, int elem) -> int
+			{
+				return LinearSearch(arr, n, elem);
+			};
 
-			auto findedIndex = LinearSearch(arr, 10, 11);
-			Assert::AreEqual(3, findedIndex);
-
-			findedIndex = LinearSearch(arr, 10, -10);
-			Assert::AreEqual(-1, findedIndex);
-
-			findedIndex = LinearSearch(arr, 10, 1);
-			Assert::AreEqual(4, findedIndex);
+			testWithArray(testing);
 		}
 
 
 		TEST_METHOD(LinearSearchTest_Vector)
 		{
-			std::vector<int> arr = { 2, 4, 9, 11, 1, 3, 6, 7, 5, 1 };
+			auto testing = [](std::vector<int>& arr, int elem) -> int
+			{
+				return LinearSearch(arr, elem);
+			};
 
-			auto findedIndex = LinearSearch(arr, 11);
-			Assert::AreEqual(3, findedIndex);
-
-			findedIndex = LinearSearch(arr, -10);
-			Assert::AreEqual(-1, findedIndex);
-
-			findedIndex = LinearSearch(arr, 1);
-			Assert::AreEqual(4, findedIndex);
+			testWithVector(testing);
 		}
 
 
 		TEST_METHOD(LinearSearchTest_VectorPointer)
 		{
-			std::vector<int> arr = { 2, 4, 9, 11, 1, 3, 6, 7, 5, 1 };
+			auto testing = [](std::vector<int>* arr, int elem) -> int
+			{
+				return LinearSearch(arr, elem);
+			};
 
-			auto findedIndex = LinearSearch(&arr, 11);
-			Assert::AreEqual(3, findedIndex);
-
-			findedIndex = LinearSearch(&arr, -10);
-			Assert::AreEqual(-1, findedIndex);
-
-			findedIndex = LinearSearch(&arr, 1);
-			Assert::AreEqual(4, findedIndex);
+			testWithVectorPointer(testing);
 		}
 	};
 }
